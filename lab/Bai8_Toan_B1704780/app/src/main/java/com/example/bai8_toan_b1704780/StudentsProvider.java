f23f432998756bf35726630057da4bf5cda57608
sillybread <quoctoan.c2b@gmail.com>
2020-11-26 20:02:21 +0700

    make bai 9

---
+++ lab/Bai8_Toan_B1704780/app/src/main/java/com/example/bai8_toan_b1704780/StudentsProvider.java
@@ -0,0 +1,4 @@
+package com.example.bai8_toan_b1704780;
+
+public class StudentsProvider {
+}
--- lab/Bai_9_Toan_B1704780/app/src/main/java/com/example/bai_9_toan_b1704780/MainActivity.java
+++ lab/Bai_9_Toan_B1704780/app/src/main/java/com/example/bai_9_toan_b1704780/MainActivity.java
@@ -1,14 +1,14 @@
-package com.example.bai7_toan_b1704780;
+package com.example.bai_9_toan_b1704780;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class XoaStudent extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_xoa_student);
+        setContentView(R.layout.activity_main);
     }
 }
